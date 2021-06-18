ec60b4d4c326d9e7a8b9785067a227379eb7e351
jayakrishnan98 <jayakrishnan98>
2021-06-18 16:06:59 +0530

    OverAll Game logic completed

--- screen/GameOverScreen.js
+++ screen/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 export default function GameOverScreen(props) {
     return (
         <View style={styles.screen}>
             <Text>Game over!!!</Text>
+            <Text>Number of Round: {props.roundsNumber}</Text>
+            <Text>Number was :{props.userNumber}</Text>
+            <Button title="New game" onPress={props.onRestart} />
         </View>
     )
 }
