53085bd8f963502f2c11b6a9db953defba87756c
jayakrishnan98 <jayakrishnan98>
2021-06-18 18:46:29 +0530

    Made the Image fit

--- screen/GameOverScreen.js
+++ screen/GameOverScreen.js
@@ -8,7 +8,7 @@ export default function GameOverScreen(props) {
     return (
         <View style={styles.screen}>
             <TitleText>Game over!!!</TitleText>
-            <Image source={require('../assets/success.png')} />
+            <Image style={styles.image} source={require('../assets/success.png')} resizeMode="contain" />
             <BodyText>Number of Round: {props.roundsNumber}</BodyText>
             <BodyText>Number was :{props.userNumber}</BodyText>
             <Button title="New game" onPress={props.onRestart} />
@@ -21,5 +21,9 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems:"center"
+    },
+    image:{
+        width:'80%',
+        height:300
     }
 })
